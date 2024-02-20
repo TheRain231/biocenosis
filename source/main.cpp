b9f9427512c8d7f883e321b7d5e5d8ce4cbbd15b
Андрей Степанов <therain3164@yandex.ru>
2024-02-20 21:48:08 +0300

    set background

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,30 @@
 #include <SFML/Graphics.hpp>
 
+#define WINDOW_WIDTH 900
+#define WINDOW_HEIGHT 900
+#define WINDOW_TITLE "Best Game"
+#define FPS 20
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "BEST GAME EVER");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+    sf::Texture background;
+    background.loadFromFile("textures/background.png");
+    sf::Sprite spriteBG;
+    spriteBG.setTexture(background);
+    spriteBG.setPosition(0, 0);
+    window.setFramerateLimit(FPS);
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        //тут начинается гейплей
-        window.clear(sf::Color::Black);
-        sf::Texture texture;
-        texture.loadFromFile("textures/cow.png");
-        sf::Sprite sprite;
-        sprite.setTexture(texture);
-
-        //размер
-        sf::Vector2f targetSize(200.0f, 200.0f);
-        sprite.setScale(
-                targetSize.x / sprite.getLocalBounds().width,
-                targetSize.y / sprite.getLocalBounds().height);
-
-        window.draw(sprite);
+        window.clear();
+        sf::Vector2f targetSize(WINDOW_HEIGHT, WINDOW_WIDTH);
+        spriteBG.setScale(
+                targetSize.x / spriteBG.getLocalBounds().width,
+                targetSize.y / spriteBG.getLocalBounds().height);
+        window.draw(spriteBG);
         window.display();
     }
     return 0;
